bcae0db0c2de800f51b2cefe6f02c1c646418f29
Byunk <clearman001@gmail.com>
2023-09-12 20:08:26 +0900

    session dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,37 @@ import express from 'express';
 import { query } from './db';
 import { budgets, documents, organizations, transactions } from './routes';
 import bodyParser from 'body-parser';
+import session from 'express-session';
+import { config } from 'dotenv';
+import RedisStore from 'connect-redis';
+import { createClient } from 'redis';
 
 query('SELECT * FROM now()', []).then((res) => {
     console.log(res.rows[0]);
 });
 
+const redisClient = createClient();
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: 'BAI:',
+    ttl: 86400,
+});
+
+config();
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+    session({
+        store: redisStore,
+        secret: process.env.SESSION_SECRET as string,
+        resave: false,
+        saveUninitialized: false,
+    }),
+);
 app.use(function (req, res, next) {
     console.log('%s %s %s', req.method, req.url, req.path);
     next();
