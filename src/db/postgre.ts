d0490291b09b2872b1f131789ea986729b1eecf2
Byunk <kyungho.byoun@gmail.com>
2023-10-14 02:18:46 +0900

    create a initdb and schema initialize function for testing

--- src/db/postgre.ts
+++ src/db/postgre.ts
@@ -1,10 +1,19 @@
-import { Sequelize } from 'sequelize';
+import { QueryTypes, Sequelize } from 'sequelize';
+import {
+    Organization,
+    User,
+    Budget,
+    Income,
+    Expense,
+    Transaction,
+    AuditPeriod,
+} from '../model';
 
-let schema_name;
-if (process.env.NODE_ENV === 'development') {
-    schema_name = 'development';
+let schema_name: string;
+if (process.env.NODE_ENV !== undefined) {
+    schema_name = process.env.NODE_ENV;
 } else {
-    schema_name = 'production';
+    schema_name = 'development';
 }
 
 console.log('schema_name: ', schema_name);
@@ -25,3 +34,36 @@ export const sequelize = new Sequelize(
 sequelize.authenticate().catch((err) => {
     console.error('Unable to connect to the database:', err);
 });
+
+export async function initDB() {
+    const models = [
+        Organization,
+        User,
+        Budget,
+        Income,
+        Expense,
+        Transaction,
+        AuditPeriod,
+    ];
+    for (const model of models) {
+        await model.sync({ force: true });
+        // await model.sync();
+        // await model.sync({ alter: true });
+    }
+
+    // Check if the schema already exists
+    const schemaExists = await sequelize.query(
+        `SELECT schema_name FROM information_schema.schemata WHERE schema_name = '${schema_name}'`,
+        {
+            type: QueryTypes.SELECT,
+        },
+    );
+
+    if (schemaExists.length === 0) {
+        // Create the schema if it doesn't exist
+        await sequelize.createSchema(schema_name, {});
+        console.log(`Schema '${schema_name}' created successfully.`);
+    } else {
+        console.log(`Schema '${schema_name}' already exists.`);
+    }
+}
