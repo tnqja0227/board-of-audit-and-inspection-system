8a9dddd58b1152e446f991ab77868e1299a0ab98
Byunk <kyungho.byoun@gmail.com>
2023-12-18 04:56:36 +0900

    update certbot.sh

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker run --rm -it -v /etc/letsencrypt:/etc/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt certbot/certbot certonly -d '*.gdsckaist.com' --manual --preferred-challenges dns --email kyungho.byoun@gmail.com --agree-tos --no-eff-email
+
+# Renew
+# docker run --rm -it -v /etc/letsencrypt:/etc/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt certbot/certbot renew -d '*.gdsckaist.com' --manual --preferred-challenges dns --email kyungho.byoun@gmail.com --agree-tos --no-eff-email
