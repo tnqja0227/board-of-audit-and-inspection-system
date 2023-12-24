f7a1920133e60fe73efa717a8e972b1dc706d532
youngmin yoo <yym3055@gmail.com>
2023-12-24 23:22:59 +0900

    Add expense_record data model.

---
+++ src/model/expense_record.ts
@@ -0,0 +1,41 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../db';
+
+class ExpenseRecord extends Model {
+    declare id: number;
+    declare transaction_id: number;
+    declare URL: string;
+    declare note: string;
+}
+
+ExpenseRecord.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        transaction_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            references: {
+                model: 'transactions',
+                key: 'id',
+            },
+        },
+        URL: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        note: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+    },
+    {
+        tableName: 'expense_records',
+        sequelize,
+    },
+);
+
+export default ExpenseRecord;
