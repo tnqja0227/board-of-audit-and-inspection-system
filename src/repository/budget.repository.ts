3ff1c21af06c3304996f0385bf27a26747518d98
Byunk <kyungho.byoun@gmail.com>
2024-01-02 21:52:10 +0900

    refactor: budgetService

--- src/repository/budget.repository.ts
+++ src/repository/budget.repository.ts
@@ -1,3 +1,4 @@
+import { sequelize } from '../db';
 import { BudgetRequestDto } from '../dto';
 import { Budget } from '../model';
 
@@ -10,6 +11,51 @@ class BudgetRepository {
                 half: dto.half,
             },
         });
+    }
+
+    async findIncomeWithSettlement(budgetId: number) {
+        const schemaName = process.env.NODE_ENV || 'development';
+        return sequelize.query(
+            `SELECT I."code", 
+                I."source", 
+                I."category", 
+                I."content", 
+                I."amount", 
+                I."note", 
+                sum(T."amount") AS "settlement"
+            FROM ${schemaName}."incomes" I 
+                LEFT JOIN ${schemaName}."transactions" T 
+                ON I."id" = T."IncomeId"
+            WHERE I."BudgetId" = ?
+            GROUP BY I."id"`,
+            {
+                type: 'SELECT',
+                replacements: [budgetId],
+            },
+        );
+    }
+
+    async findExpenseWithSettlement(budgetId: number) {
+        const schemaName = process.env.NODE_ENV || 'development';
+        return sequelize.query(
+            `SELECT E."code", 
+                E."source", 
+                E."category", 
+                E."content", 
+                E."project", 
+                E."amount", 
+                E."note", 
+                sum(T."amount") AS "settlement"
+            FROM ${schemaName}."expenses" E 
+                LEFT JOIN ${schemaName}."transactions" T 
+                ON E."id" = T."ExpenseId"
+            WHERE E."BudgetId" = ?
+            GROUP BY E."id"`,
+            {
+                type: 'SELECT',
+                replacements: [budgetId],
+            },
+        );
     }
 
     async createBudget(dto: BudgetRequestDto, manager: string) {
--- src/service/budget.service.ts
+++ src/service/budget.service.ts
@@ -1,5 +1,4 @@
 import logger from '../config/winston';
-import { sequelize } from '../db';
 import { BudgetRepository, OrganizationRepository } from '../repository';
 import { BudgetRequestDto } from '../dto';
 import { NotFoundError } from '../utils/errors';
@@ -43,7 +42,7 @@ class BudgetService {
 
         const budget = await this.findBudget(dto);
 
-        const incomeWithSettlement = await this.getIncomeWithSettlement(
+        const incomeWithSettlement = await this.budgetRepository.findIncomeWithSettlement(
             budget.id,
         );
         const formattedIncome = this.groupBy(
@@ -127,10 +126,10 @@ class BudgetService {
                 `${dto.organizationId} 피감기관이 존재하지 않습니다`,
             );
         }
-        
+
         const budget = await this.findBudget(dto);
 
-        const expenseWithSettlement = await this.getExpenseWithSettlement(
+        const expenseWithSettlement = await this.budgetRepository.findExpenseWithSettlement(
             budget.id,
         );
         const formattedExpense = this.groupBy(
@@ -290,51 +289,6 @@ class BudgetService {
             본회계: getTotalBySource('본회계'),
             자치: getTotalBySource('자치'),
         };
-    }
-
-    private async getIncomeWithSettlement(budget_id: number) {
-        const schemaName = process.env.NODE_ENV || 'development';
-        return sequelize.query(
-            `SELECT I."code", 
-                I."source", 
-                I."category", 
-                I."content", 
-                I."amount", 
-                I."note", 
-                sum(T."amount") AS "settlement"
-            FROM ${schemaName}."incomes" I 
-                LEFT JOIN ${schemaName}."transactions" T 
-                ON I."id" = T."IncomeId"
-            WHERE I."BudgetId" = ?
-            GROUP BY I."id"`,
-            {
-                type: 'SELECT',
-                replacements: [budget_id],
-            },
-        );
-    }
-
-    private async getExpenseWithSettlement(budget_id: number) {
-        const schemaName = process.env.NODE_ENV || 'development';
-        return sequelize.query(
-            `SELECT E."code", 
-                E."source", 
-                E."category", 
-                E."content", 
-                E."project", 
-                E."amount", 
-                E."note", 
-                sum(T."amount") AS "settlement"
-            FROM ${schemaName}."expenses" E 
-                LEFT JOIN ${schemaName}."transactions" T 
-                ON E."id" = T."ExpenseId"
-            WHERE E."BudgetId" = ?
-            GROUP BY E."id"`,
-            {
-                type: 'SELECT',
-                replacements: [budget_id],
-            },
-        );
     }
 
     // array를 key 기준으로 group한다.
