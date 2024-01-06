a01b6ae5e09b31600b3affc533e13d03fd4a8ff4
youngmin yoo <yym3055@gmail.com>
2024-01-06 20:06:40 +0900

    ADD upload to S3

--- src/service/s3.ts
+++ src/service/s3.ts
@@ -8,37 +8,53 @@ export async function uploadFileToS3(filePath: any) {
     // file: local temp file path. After invoking this function successfully, delete the temp file.
     logger.info('Uploading file to S3...');
     logger.info(filePath);
-    logger.info('File uploaded to S3');
+
+    // 10MB: AWS API Gateway limit
+    if (
+        !fs.existsSync(filePath) ||
+        fs.statSync(filePath).size > 10 * 1024 * 1024
+    ) {
+        throw new Error('File does not exist or is larger than 10MB');
+    }
+
     // upload file to s3 and return the url and status code
     const fileContent = fs.readFileSync(filePath);
     const fileName = path.basename(filePath);
     const fileTypeResult = await fileType.fileTypeFromBuffer(fileContent);
 
+    const allowedFileTypes = [
+        'image/jpeg',
+        'image/png',
+        'image/jpg',
+        'image/webp',
+        'application/pdf',
+    ];
     let contentType = 'text/plain';
-    if (fileTypeResult) {
+    if (fileTypeResult && allowedFileTypes.includes(fileTypeResult.mime)) {
         const contentType = fileTypeResult.mime;
     } else {
         const contentType = 'text/plain';
         logger.warn('fileTypeResult is null. Defaulting to text/plain');
     }
 
-    const apiGatewayFullURL = `${process.env.AWS_S3_API_GATEWAY_URL}/${fileName}`;
+    const apiGatewayFullURL = `${process.env.AWS_S3_API_GATEWAY_URL}/${process.env.AWS_S3_BUCKET_NAME}/${fileName}`;
 
     try {
-        const response = await axios.put(apiGatewayFullURL, fileContent, {
+        const AWSresponse = await axios.put(apiGatewayFullURL, fileContent, {
             headers: {
                 'Content-Type': contentType,
             },
         });
 
-        logger.info(response);
+        logger.info('File uploaded to S3');
+        logger.info(AWSresponse);
         const ret = {
             url: apiGatewayFullURL,
-            statusCode: response.status,
+            statusCode: AWSresponse.status,
         };
         return ret;
     } catch (error) {
-        logger.error(error);
+        logger.error('error uploading to s3: ', error);
         throw error; // TODO: Error handling
     }
 }
