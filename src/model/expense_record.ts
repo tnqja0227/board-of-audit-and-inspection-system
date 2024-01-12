39cc29f65006fc50ff94945f0f701cd460f16e58
youngmin yoo <yym3055@gmail.com>
2024-01-12 22:02:59 +0900

    Add comments

--- src/model/expense_record.ts
+++ src/model/expense_record.ts
@@ -3,9 +3,9 @@ import { sequelize } from '../db';
 
 class ExpenseRecord extends Model {
     declare id: number;
-    declare transactionId: number;
-    declare key: string;
-    declare note: string;
+    declare transactionId: number; // 통장 거래 내역 id
+    declare key: string; // s3 bucket 상에서 이미지의 key
+    declare note: string; // 비고
 }
 
 ExpenseRecord.init(
