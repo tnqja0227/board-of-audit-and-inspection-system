79af45ad692cbab02be309d249f40037433256b3
youngmin yoo <yym3055@gmail.com>
2024-01-28 21:02:58 +0900

    Add routers for card evidence and account records

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,8 @@ import { createTestRouter } from './test';
 import { createAccountsRouter } from './account';
 import { createBudgetsRouter } from './budget';
 import { createTransactionRecordsRouter } from './transaction_record';
+import { createCardEvidenceRouter } from './card_evidence';
+import { createAccountRecordRouter } from './account_record';
 
 export function createRouter() {
     const router = express.Router();
@@ -16,6 +18,7 @@ export function createRouter() {
     router.use('/users', createUsersRouter());
     router.use('/test', createTestRouter());
     router.use('/transaction_records', createTransactionRecordsRouter());
-
+    router.use('/card_evidences', createCardEvidenceRouter());
+    router.use('/account_records', createAccountRecordRouter());
     return router;
 }
